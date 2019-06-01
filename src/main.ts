ae4bd5b16494f07fa7d5078f0ead6c6cd3ab5564
spdy999 <1a1a1a.262626.0010@gmail.com>
2019-06-01 12:46:43 +0700

    remove db connection from main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { dbConnection } from './pg-connection';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-
-  await dbConnection();
 
   await app.listen(PORT);
   Logger.log(`Server is running on PORT ${PORT}`);
